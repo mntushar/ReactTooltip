51d2d6989ea0b7fbaf23d77c181a1dcb36c77135
M N Tushar <45463161+mntushar@users.noreply.github.com>
2025-03-11 11:59:35 +0600

    Update Example.tsx

--- Example.tsx
+++ Example.tsx
@@ -3,14 +3,41 @@ import Tooltip from "./tooltip";
 export default function Example() {
     return (
         <Fragment>
-            <Tooltip text="Image">
+            <Tooltip text="Image" possition='left'>
                     <Image
                       src='/images/icons8-delete-128.png'
                       alt='icons8-delete-128.png'
                       width={25}
                       height={23}
                     />
-                  </Tooltip>
+            </Tooltip>
+
+            <Tooltip text="Image" possition='right'>
+                    <Image
+                      src='/images/icons8-delete-128.png'
+                      alt='icons8-delete-128.png'
+                      width={25}
+                      height={23}
+                    />
+            </Tooltip>
+
+            <Tooltip text="Image" possition='top'>
+                    <Image
+                      src='/images/icons8-delete-128.png'
+                      alt='icons8-delete-128.png'
+                      width={25}
+                      height={23}
+                    />
+            </Tooltip>
+
+            <Tooltip text="Image" possition='bottom'>
+                    <Image
+                      src='/images/icons8-delete-128.png'
+                      alt='icons8-delete-128.png'
+                      width={25}
+                      height={23}
+                    />
+            </Tooltip>
         </Fragment>
     );
 }
